eeb94e4bf3d290d0dd21c255e24bf1165f5f1b25
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2020-12-19 17:02:09 +0800

    data block added

---
+++ src/app/modules/dashboard/data.js
@@ -0,0 +1,65 @@
+
+
+const data = [
+  {
+    name: 'Concept Tower',
+    rooms: [
+      {
+        name: '101',
+        value: 50,
+        widgetColor: '#a8edb5',
+        status: true,
+      },
+      {
+        name: '102',
+        value: 90,
+        widgetColor: '#edc1a8',
+        status: true,
+      },
+      {
+        name: '103',
+        value: 30,
+        widgetColor: '#d1d1d1',
+        status: false,
+      },
+      {
+        name: '104',
+        value: 10,
+        widgetColor: '#a8bded',
+        status: true,
+      }
+    ]
+  },
+  {
+    name: 'Reply Square',
+    rooms: [
+      {
+        name: '201',
+        value: 90,
+        widgetColor: '#edc1a8',
+        status: true,
+      },
+      {
+        name: '202',
+        value: 30,
+        widgetColor: '#a8edd1',
+        status: true,
+      },
+      {
+        name: '203',
+        value: 20,
+        widgetColor: '#a8bded',
+        status: true,
+      },
+
+      {
+        name: '204',
+        value: 10,
+        widgetColor: '#a8bded',
+        status: true,
+      }
+    ]
+  }
+]
+
+export default data;
